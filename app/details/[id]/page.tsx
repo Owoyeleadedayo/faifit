24f4b5cae6ec49a905f448a246fe435e0668d9d4
Owoyele Adedayo <67150149+Owoyeleadedayo@users.noreply.github.com>
2025-08-23 10:29:42 -0700

    updated my code

--- app/details/[id]/page.tsx
+++ app/details/[id]/page.tsx
@@ -1,23 +1,53 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { Flex, Image, Text, Button } from "@chakra-ui/react";
+import { Flex, Image, Text, Button, Select } from "@chakra-ui/react";
 import { ShoppingCart } from "lucide-react";
 import { useCart } from "@/app/context/CartContext";
 import NavBarforOthers from "@/app/components/NavBarforOthers";
 
 // Dummy product data (same as in StorePage)
 const productCard = [
-  { id: 1, img: "/images/img2.jpg", title: "Oman wor tawa", price: "85,000 00" },
-  { id: 2, img: "/images/img6.jpg", title: "Oman wor tawa", price: "85,000 00" },
-  { id: 3, img: "/images/img11.jpg", title: "Oman wor tawa", price: "85,000 00" },
-  { id: 4, img: "/images/img12.jpg", title: "Oman wor tawa", price: "85,000 00" },
-  { id: 5, img: "/images/img13.jpg", title: "Oman wor tawa", price: "85,000 00" },
-  { id: 6, img: "/images/img14.jpg", title: "Oman wor tawa", price: "85,000 00" },
+  {
+    id: 1,
+    img: "/images/img2.jpg",
+    title: "Oman wor tawa",
+    price: "85,000 00",
+  },
+  {
+    id: 2,
+    img: "/images/img6.jpg",
+    title: "Oman wor tawa",
+    price: "85,000 00",
+  },
+  {
+    id: 3,
+    img: "/images/img11.jpg",
+    title: "Oman wor tawa",
+    price: "85,000 00",
+  },
+  {
+    id: 4,
+    img: "/images/img12.jpg",
+    title: "Oman wor tawa",
+    price: "85,000 00",
+  },
+  {
+    id: 5,
+    img: "/images/img13.jpg",
+    title: "Oman wor tawa",
+    price: "85,000 00",
+  },
+  {
+    id: 6,
+    img: "/images/img14.jpg",
+    title: "Oman wor tawa",
+    price: "85,000 00",
+  },
 ];
 
 export default function ProductDetails() {
-    const { addToCart } = useCart();
+  const { addToCart } = useCart();
   const params = useParams();
   const id = params?.id; // comes as string
 
@@ -35,47 +65,119 @@ export default function ProductDetails() {
 
   return (
     <>
-        <NavBarforOthers />
-        <Flex
-      w="100%"
-      minH="100vh"
-      p="30px"
-      flexDirection={{ base: "column", md: "row" }}
-      gap="40px"
-    >
-      <Image
-        src={product.img}
-        alt={product.title}
-        objectFit="cover"
-        w={{ base: "100%", md: "50%" }}
-        h="400px"
-      />
+      <NavBarforOthers />
+      <Flex
+        w="100%"
+        minH="100vh"
+        px="30px"
+        pt={"80px"}
+        flexDirection={{ base: "column", md: "row" }}
+        gap="40px"
+      >
+        <Flex>
+          <Image
+            src={product.img}
+            alt={product.title}
+            objectFit="contain"
+            w={{ base: "100%", md: "50%", lg: "500px" }}
+            h="600px"
+          />
+        </Flex>
 
-      <Flex flexDirection="column" justify="center" gap="20px" w={{ base: "100%", md: "50%" }}>
-        <Text fontSize="28px" fontWeight="600">{product.title}</Text>
-        <Text fontSize="20px" fontWeight="500">Price: ₦{product.price}</Text>
+        <Flex flexDirection="column" gap="10px" w={{ base: "100%", md: "50%" }} pt="50px">
+          <Flex
+            flexDirection="column"
+            w={{ base: "100%", md: "50%" }}
+            py="10px"
+            borderBottom="1px solid #e5e5e5"
+            gap="5px"
+          >
+            <Text fontSize="18px" fontWeight="500" textTransform={"uppercase"}>
+              {product.title}
+            </Text>
+            <Text fontSize="16px" fontWeight="400">
+              ₦{product.price}
+            </Text>
+          </Flex>
 
-        <Button
-          bg="#000"
-          color="white"
-          fontSize="16px"
-          fontWeight="500"
-          leftIcon={<ShoppingCart size={18} />}
-          _hover={{ bg: "gray.800" }}
-          w="200px"
-          onClick={() =>
-            addToCart({
-              id: product.id,
-              title: product.title,
-              img: product.img,
-              price: product.price,
-            })
-          }
-        >
-          Add to Cart
-        </Button>
+          <Flex width={"100%"} flexDirection={"column"} gap="10px">
+            <Flex width={"50%"} flexDirection={"column"} gap="5px">
+              <Text
+                fontSize="14px"
+                fontWeight="500"
+                textTransform={"uppercase"}
+              >
+                Color
+              </Text>
+              <Select
+                width={"100%"}
+                variant="none"
+                border={"1px solid #e5e5e5"}
+                fontSize="16px"
+                fontWeight="400"
+                textTransform={"capitalize"}
+                borderRadius="none"
+                _hover={{
+                  border: "1px solid black",
+                }}
+                placeholder="Select option"
+              >
+                <option value="pink">Pink</option>
+                <option value="blue">Blue</option>
+                <option value="orange">Orange</option>
+                <option value="black">Black</option>
+              </Select>
+            </Flex>
+            <Flex width={"50%"} flexDirection={"column"} gap="5px">
+              <Text
+                fontSize="14px"
+                fontWeight="500"
+                textTransform={"uppercase"}
+              >
+                size
+              </Text>
+              <Select
+                width={"100%"}
+                variant="none"
+                border={"1px solid #e5e5e5"}
+                fontSize="16px"
+                fontWeight="400"
+                textTransform={"capitalize"}
+                borderRadius="none"
+                _hover={{
+                  border: "1px solid black",
+                }}
+                placeholder="Select option"
+              >
+                <option value="s">S</option>
+                <option value="m">M</option>
+                <option value="l">L</option>
+                <option value="xl">XL</option>
+              </Select>
+            </Flex>
+          </Flex>
+
+          <Button
+            bg="#000"
+            color="white"
+            fontSize="16px"
+            fontWeight="500"
+            leftIcon={<ShoppingCart size={18} />}
+            _hover={{ bg: "gray.800" }}
+            w="200px"
+            onClick={() =>
+              addToCart({
+                id: product.id,
+                title: product.title,
+                img: product.img,
+                price: product.price,
+              })
+            }
+          >
+            Add to Cart
+          </Button>
+        </Flex>
       </Flex>
-    </Flex>
     </>
   );
 }
