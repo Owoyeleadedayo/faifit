25744214582e5dcbb45486808fcbdcc7016748c9
Owoyele Adedayo <67150149+Owoyeleadedayo@users.noreply.github.com>
2025-08-29 04:20:35 -0700

    trying to fix all

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ["twzaxpwgrivrbhirhbpk.supabase.co"],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path((?!details).*)', // Match all paths except those starting with /details
+        destination: '/',
+        permanent: false,
+      },
+    ];
   },
 };
 
